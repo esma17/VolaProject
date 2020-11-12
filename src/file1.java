9fbef928a283e5effc1c428b9539a10dbcac08ef
esma17 <milkysf@gmail.com>
2020-11-12 09:48:32 -0600

    working nice message2

--- src/file1.java
+++ src/file1.java
@@ -2,5 +2,7 @@ public class file1 {
     public static void main(String[] args) {
         System.out.println("Hello Git last day");
         //B21 is excited!!!
+        //B21 is dedicated
+
     }
 }
